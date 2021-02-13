ed3b82ff56477fc2c0c9e2daffd36f4b6b66126b
João Victor <jv.martins@MacBook-Air-de-Joao.local>
2021-02-13 13:51:57 -0300

    Programa responsável por conter as configuraçoões dos serviços

---
+++ src/test/java/br/com/caelum/eatsconfig/eatsconfig/EatsConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.caelum.eatsconfig.eatsconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatsConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
